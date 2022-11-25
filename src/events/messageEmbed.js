3502ce53f4303e22bebe6b7781b7668a5b409ed0
tahapek5454 <tahapek5454@gmail.com>
2022-11-25 14:26:50 +0300

    add EmbedBuilder

--- src/events/messageEmbed.js
+++ src/events/messageEmbed.js
@@ -5,11 +5,15 @@ export default client =>{
         if (message.content == "bilgi"){
             // reply botun yazacagı sey
             var response = new EmbedBuilder()
-            .setDescription("Ben bu kanala hizmet etmek için varım")
+            .setDescription("Ben bu kanala hizmet etmek için varım\n\u200b")
             .setColor("Orange")
             .setAuthor({name:"Test Botu", iconURL : client.user.displayAvatarURL(), url:"https://github.com/tahapek5454"})
             .setFooter({text:"Test Botu Hizmetinizdedir."})
             .setTimestamp()
+            .addFields(
+                {name : "Developer", value: "Taha Pek", inline:true},
+                {name: "\u200b", value:"[Gihtub Hesabım](https://github.com/tahapek5454)", inline:true})
+
             message.channel.send({content:"Benim Hakkımdaki Bilgiler Şu Şekildedir", embeds:[response]})
             
         }
