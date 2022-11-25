0911833cfb43d906dda4b019d26aa251ea2fdd96
tahapek5454 <tahapek5454@gmail.com>
2022-11-25 15:47:46 +0300

    update messageCreate

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,13 +1,38 @@
 
 // burası messajları algılayıp cevap verdiğimiz event handler
 export default client =>{
+    const prefix = process.env.PREFIX
+    // bunu koyma amacımız kalıplaşmış bot komutlarını oluşturmak
+    console.log(prefix)
     client.on("messageCreate",(message)=>{
-        if (message.content == "merhaba"){
-            // reply botun yazacagı sey
-            message.reply('Merhabalar Test Kanalına Hoşgeldiniz')
-            .then(() => console.log(`Belitilen mesaja başarılı şekilde cevap verildi "${message.content}"`))
-            .catch(console.error);
-        }
+
+        if (message.content.startsWith(prefix) == false) return
+        // prefixle başlamıyorsa kale alma
+
+        const args = message.content.slice(prefix.length).trim().split(/ +/)
+        // biz gelen komutu parametrelerini de almak için ayırma işlemini yaptık
+
+        const commanName = args.shift().toLowerCase()
+        // shift ile dizideki ilk elemanı alıp kalan elemanları diziye attık
+
+        const command = client.commands.get(commanName)
+
+        if(!command) return
+        // eger belitrilen adda bir command bulunmuyorsa return at
+
+         try {
+            // eger sıkınıt yoksa commandsmiz içindeki fonksiyonu calistiralim
+            command.execute(message)
+            
+         } catch (error) {
+            console.log(error)
+            message.reply("İlgili Komutta Hata Bulunmaktadır.")
+            
+         }
+        // burada program çalışırken eklenmiş olan comman collectionun içindeki
+        // ilgili command bizim degiskenimize atanıyor
+
+        
     })
 }
 
